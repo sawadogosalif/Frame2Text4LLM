c5ef7fe07c7c2ca9ac53932beca13b82d22f8ba8
Salif SAWADOGO <96554481+sawadogosalif@users.noreply.github.com>
2025-03-28 23:18:05 +0100

    :tada: initialisation

---
+++ HISTORY.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## [0.0.1] - 2024-MM-DD
+### Added
+- Initial release
+
+### Known Limitations
+
+
+## [0.0.2] - 2025-MM-DD
---
+++ src/frame2text4llm/package_metadata.py
@@ -0,0 +1,8 @@
+"""Package metadata. """
+
+__doc__ = "Le projet frame2text4llm consiste à extraire du texte à partir de frames vidéo via OCR, afin d'exploiter ce texte pour des taches de NLP /LLM si on veut etre buzzyword :)."
+__name__ = "frame2text4llm à confirmer "
+__author__ = "Alban NYANTUDRE, Salif SAWADOGO"
+__email__ = "frame2text4llm@gmail.com"
+__version__ = "0.0.1"
+__url__ = "https://github.com/sawadogosalif/Frame2Text4LLM"
