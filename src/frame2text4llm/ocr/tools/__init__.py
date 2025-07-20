bffbf333077bb9f03c8f32cf6253ca0a75de3cf6
SALFO SAWALLE <96554481+sawallesalfo@users.noreply.github.com>
2025-07-20 20:07:09 +0200

    try to rmeove tesseract

--- src/frame2text4llm/ocr/tools/__init__.py
+++ src/frame2text4llm/ocr/tools/__init__.py
@@ -3,14 +3,10 @@ from .tesseract import TesseractOCR
 from .paddleocr import PaddleOCR
 from .openaiocr import OpenAIOCR
 from .mistralocr import MistralOCR
-from .easyocr import EasyOCR
-from .vlmocr import VLMOCR
 
 OCR_TOOLS = {
     "tesseract": TesseractOCR,
     "paddleocr": PaddleOCR,
     "openai": OpenAIOCR,
     "mistral": MistralOCR,
-    "easyocr": EasyOCR,
-    "vlm": VLMOCR,
 }
